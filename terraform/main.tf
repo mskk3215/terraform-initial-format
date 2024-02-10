b666344575c28958963539e05e5dc2b452e90031
mskk3215 <m.kaaiweb@gmail.com>
2024-02-10 14:14:06 +0900

    feat:tfstate file moved to S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,12 @@ terraform {
       version = ">= 3.0"
     }
   }
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-ravel"
+    key     = "tastylog.dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
 }
 # ----------------------
 # Provider
