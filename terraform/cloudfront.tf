75b10d274940b6cd42bd19ccc6e4f2259953c7b4
mskk3215 <m.kaaiweb@gmail.com>
2024-02-11 15:58:01 +0900

    feat:CloudFront add

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,96 @@
+# ----------------------
+# CloudFront cache distribution
+# ----------------------
+# CloudFront
+resource "aws_cloudfront_distribution" "cf" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "cache distribution"
+  price_class     = "PriceClass_All"
+  # オリジン
+  # ALB用オリジン
+  origin {
+    domain_name = aws_route53_record.route53_record.fqdn
+    origin_id   = aws_lb.alb.name
+    custom_origin_config {
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      http_port              = 80
+      https_port             = 443
+    }
+  }
+  # S3用オリジン
+  origin {
+    domain_name = aws_s3_bucket.s3_static_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_static_bucket.id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cf_s3_origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  # ビヘイビア
+  # ALB用ビヘイビア
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "all"
+      }
+    }
+    target_origin_id       = aws_lb.alb.name
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+  # S3用ビヘイビア
+  ordered_cache_behavior {
+    path_pattern     = "/public/"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.s3_static_bucket.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+  }
+  # アクセス制限
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  aliases = ["dev.${var.domain}"]
+  # 証明書
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.virginia_cert.arn
+    minimum_protocol_version = "TLSv1.2_2019"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+# s3へアクセスする為のアイデンティティ
+resource "aws_cloudfront_origin_access_identity" "cf_s3_origin_access_identity" {
+  comment = "s3 static bucket access identity"
+}
+
+# route53 record
+resource "aws_route53_record" "route53_cloudfront" {
+  zone_id = aws_route53_zone.route53_zone.zone_id
+  name    = "dev.${var.domain}"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.cf.domain_name
+    zone_id                = aws_cloudfront_distribution.cf.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
