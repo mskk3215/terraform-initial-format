793c47e334665d410a454e32417391645915132a
mskk3215 <m.kaaiweb@gmail.com>
2024-02-11 19:44:39 +0900

    feat:AutoScaling add

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,14 +7,6 @@ data "aws_ami" "app" {
   owners      = ["self", "amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["tastylog-*-ami"]
   }
 }
